350efbb7043130a5ceae3650933a096806b07278
Sarah Janzen <cadominj@gmail.com>
2025-10-21 17:06:53 -0700

    onchange callback = clean + update

--- modules/HST/MediByte.js
+++ modules/HST/MediByte.js
@@ -4,8 +4,13 @@ console.log("MediByte.js");
 
 script.data[script.key] = {};
 
-// callback fn when data is extracted from a field (no changes)
-script.data[script.key]["clean"] = {
+// ids that have no onchange callback fn
+script.data[script.key].no_change = [
+  "scored_at",
+];
+
+// callback fn when data is extracted from a field
+script.data[script.key].clean = {
   "scored_at": () => script.clip_percent(scored_at.value, 0, 3, 4),
   "duration": () => script.clip_minutes(duration.value),
   "ahi": () => script.clip_index(ahi.value),
@@ -23,28 +28,10 @@ script.data[script.key]["clean"] = {
   "snores": () => script.clip_number(snores.value, 0, 0),
 };
 
-// onchange callback fn
-script.data[script.key]["update"] = {
-  "duration": () => duration.value = script.clip_minutes(duration.value),
-  "ahi": () => {
-    ahi.value = script.clip_index(ahi.value);
-    update_ai(ahi, ai, hi);
-  },
-  "ai": () => ai.value = script.clip_index(ai.value), // calculate
-  "hi": () => {
-    hi.value = script.clip_index(hi.value);
-    update_ai(ahi, ai, hi);
-  },
-  "s_ahi": () => s_ahi.value = script.clip_index(s_ahi.value),
-  "s_percent": () => s_percent.value = script.clip_percent(s_percent.value),
-  "ox_avg": () => ox_avg.value = script.clip_percent(ox_avg.value),
-  "ox_min": () => ox_min.value = script.clip_percent(ox_min.value),
-  "odi": () => odi.value = script.clip_index(odi.value),
-  "od_duration": () => od_duration.value = script.clip_minutes(od_duration.value),
-  "pulse_min": () => pulse_min.value = script.clip_number(pulse_min.value, 1, 0),
-  "pulse_avg": () => pulse_avg.value = script.clip_number(pulse_avg.value, 1, 0),
-  "pulse_max": () => pulse_max.value = script.clip_number(pulse_max.value, 1, 0),
-  "snores": () => snores.value = script.clip_number(snores.value, 0, 0),
+// update function that is called after clean in onchange callback fn
+script.data[script.key].update = {
+  "ahi": () => {update_ai(ahi, ai, hi);},
+  "hi": () => {update_ai(ahi, ai, hi);},
 };
 
 function update_ai(ahi, ai, hi) {
