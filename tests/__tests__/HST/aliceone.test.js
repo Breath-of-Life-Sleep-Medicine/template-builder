6f17d1c45d8354d047cb3b0f94ffbcfd5fd0c16d
Sarah Janzen <cadominj@gmail.com>
2025-11-24 00:19:02 -0800

    update: aliceone test

--- tests/__tests__/HST/aliceone.test.js
+++ tests/__tests__/HST/aliceone.test.js
@@ -2,36 +2,37 @@
  * @jest-environment jsdom
  */
 
-import { data, key, key_global } from "../../../modules/data";
-import {get_paths, get_lines, find_replace, get_file_str, build_form, init_data, update_calculated} from "/tests/util.js";
+import { key, key_global } from "/modules/data.js";
+import * as tst from "/tests/util.js";
+import { get_map } from "/script.js";
 
 // sets data callback functions
 beforeAll(async () => {
-  init_data();
+  tst.init_data();
   await import ("/modules/index.js");
   await import("/modules/HST/AliceOne.js");
 });
 
-// aliceone test
-test("aliceone find_replace", () => {
-  let path = "HST/AliceOne";
-  let {template, expected} = get_paths(path);
+function update() {
+  tst.update_calculated({changed: "start", calculated: ["end"]}); // update end
+  tst.update_calculated({changed: "ahi", calculated: ["ai"]});    // update ai
+}
 
-  build_form({
+function setup_valid() {
+  tst.build_form({
     date: "2025-01-20",
     referring: "Example Doctor PAC",
     provider: "Rotcod Elpmaxe FNP",
   }, key_global);
-
-  build_form({
+  tst.build_form({
     start: "22:00", // 10:00 PM
-    trt: {m:360.0},
+    trt: "360.0",
     ahi: "25.0",
     hi: "20.0",
     ox_avg: "96",
     ox_min: "80",
     odi: "2.4",
-    od_duration: {m:5.4},
+    od_duration: "5.4",
     pulse_min: "55",
     pulse_avg: "76.5",
     pulse_max: "110",
@@ -40,9 +41,59 @@ test("aliceone find_replace", () => {
     end: "00:00", // 4:00 AM
     ai: "", // 5.0
   });
+  update();
+}
 
-  update_calculated({changed: "start", calculated: ["end"]}); // update end
-  update_calculated({changed: "ahi", calculated: ["ai"]});    // update ai
+function setup_empty() {
+  tst.build_form({
+    date: "",
+    referring: "",
+    provider: "",
+  }, key_global);
+  tst.build_form({
+    start: "",
+    trt: "",
+    ahi: "",
+    hi: "",
+    ox_avg: "",
+    ox_min: "",
+    odi: "",
+    od_duration: "",
+    pulse_min: "",
+    pulse_avg: "",
+    pulse_max: "",
+    snores: "",
+    // calculated
+    end: "",
+    ai: "",
+  });
+}
 
-  expect(get_lines(find_replace(template))).toStrictEqual(get_lines(get_file_str(expected))); // ignore newline
+test("find_replace", () => {
+  let path = "HST/AliceOne";
+  let {template, expected} = tst.get_paths(path);
+  setup_valid();
+  expect(tst.get_lines(tst.find_replace(template))).toStrictEqual(tst.get_lines(tst.get_file_str(expected))); // ignore newline
+});
+
+test("empty form", () => {
+  setup_empty();
+  let expected = {
+    start:       "12:00 AM",
+    trt:         "0.0 minutes",
+    ahi:         "0.0",
+    hi:          "0.0",
+    ox_avg:      "0",
+    ox_min:      "0",
+    odi:         "0.0",
+    od_duration: "0.0 minutes",
+    pulse_min:   "0",
+    pulse_avg:   "0.0",
+    pulse_max:   "0",
+    snores:      "0",
+    // calculated
+    end:         "12:00 AM",
+    ai:          "0.0",
+  };
+  expect(get_map(key)).toEqual(expected);
 });
