4fa23a2daec35db069c4abe0737a7b796cae0b26
Sarah Janzen <cadominj@gmail.com>
2025-12-07 19:13:14 -0800

    alice one: added scored_at

--- modules/HST/AliceOne.js
+++ modules/HST/AliceOne.js
@@ -1,7 +1,8 @@
 import { data, key, Defaults } from "../data.js";
-import { update_end } from "../form.js";
+import { update_end, update_scored_at } from "../form.js";
 
 data[key].data = {
+  scored_at:   Defaults.percent({value:4, precision:0, min:3, max:4}),
   start:       Defaults.time(),
   end:         Defaults.time(),                 // calculate
   trt:         Defaults.minutes(),
@@ -17,7 +18,11 @@ data[key].data = {
   ...data[key].data, // only set things that aren't already set
 }
 
+const DATA = data[key].data;
+DATA.scored_at.clean.change = false;
+
 data[key].update = {
+  "scored_at": update_scored_at,
   "start": () => {update_end(end)},
   "trt": () => {update_end(end)},
   "ahi": () => {update_ai(ahi, ai, hi);},
