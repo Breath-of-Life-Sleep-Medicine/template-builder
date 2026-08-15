ccad6f7cdb50e594357e8e67d74c6a811042a84c
Cadomin Janzen <cadominj@gmail.com>
2026-08-14 19:29:15 -0700

    watchpat module: added calculations, fixed template setters, fixed scored_at

--- modules/HST/WatchPAT.js
+++ modules/HST/WatchPAT.js
@@ -2,7 +2,7 @@ import { data, key, Defaults } from "../data.js";
 
 
 data[key].data = {
-  scored_at: Defaults.percent({value:4, precision:0, min:3, max:4}),
+  scored_at: Defaults.percent({value:3, precision:0, min:3, max:4}),
   epworth: Defaults.index({precision: 0, min: 0, max: 24}),
   stopbang: Defaults.index({precision: 0, min: 0, max: 8}),
   bmi: Defaults.index({precision: 1}),
@@ -38,18 +38,32 @@ let DATA = data[key].data;
 
 // non-default template setters
 data[key].template_set = {
-  epworth:  () => epworth_na.checked ? "[]" : DATA.epworth.value.toStr(),
-  stopbang: () => stopbang_na.checked ? "[]" : DATA.stopbang.value.toStr(),
-  bmi:      () => bmi_na.checked ? "[]" : DATA.bmi.value.toStr(),
+  epworth:  () => epworth_na.checked ? "[]" : DATA.epworth.template.set("epworth"),
+  stopbang: () => stopbang_na.checked ? "[]" : DATA.stopbang.template.set("stopbang"),
+  bmi:      () => bmi_na.checked ? "[]" : DATA.bmi.template.set("bmi"),
   afib:     () => afib_na.checked ? "not detected" : "detected; total duration: " + DATA.afib_duration.value.toStr(),
 }
 
 // non-default onchange callback fns
 data[key].update = {
+  // NA toggles
   epworth_na:  () => {toggle('epworth_visibility');},
   stopbang_na: () => {toggle('stopbang_visibility');},
   bmi_na:      () => {toggle('bmi_visibility');},
   afib_na:     () => {toggle('afib_visibility');},
+  // update calculated fields
+  start: update_duration,
+  end: update_duration,
+  ahi: update_cahi_percent,
+  cahi: update_cahi_percent,
+  tst: () => {
+    update_duration_percent(s_duration_percent, "s_duration_percent", s_duration_min);
+    update_duration_percent(od_percent, "od_percent", od_duration);
+    update_duration_percent(snore_percent, "snore_percent", snore_min);
+  },
+  s_duration_min: () => {update_duration_percent(s_duration_percent, "s_duration_percent", s_duration_min)},
+  od_duration:    () => {update_duration_percent(od_percent, "od_percent", od_duration)},
+  snore_min:      () => {update_duration_percent(snore_percent, "snore_percent", snore_min)},
 };
 
 function toggle(toggle_class) {
@@ -57,4 +71,23 @@ function toggle(toggle_class) {
   for (let element of elements) {
     element.hidden = !element.hidden;
   }
+}
+
+// calculations for calculated fields
+
+function update_duration() {
+  // duration = end - start
+  DATA.trt.value.set_dt(start.value, end.value);
+  DATA.trt.clean.fn(DATA.trt.value, "trt"); // dumb, but clean does extra stuff
+}
+
+function update_cahi_percent() {
+  // cahi % = cahi / ahi
+  let val = Number(DATA.cahi.value) / Number(DATA.ahi.value);
+  DATA.cahi_percent.clean.fn(val, "cahi_percent");
+}
+
+// get % of minutes / tst
+function update_duration_percent(percent, percent_str, minutes) {
+  DATA[percent_str].clean.fn(100.0 * (Number(minutes)/Number(DATA.tst.value.toMinutes())), percent_str);
 }
